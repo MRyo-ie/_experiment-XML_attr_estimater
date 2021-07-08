00bad263c95e069e21e4e8e7526812c150fa3dca
MRyo-ie <k208578@ie.u-ryukyu.ac.jp>
2021-07-08 17:06:54 +0900

    [v0.72]「リファクタリング」
    ・ 実験パラメータを json に出力するコードを追加。
    ・ モデルの save() を修正。

--- Experiment/base_ExpTrain.py
+++ Experiment/base_ExpTrain.py
@@ -2,25 +2,6 @@ import os
 import random
 import time
 from tqdm import tqdm
-# from livelossplot import PlotLosses
-# %matplotlib inline
-
-# from utils.Logger import showPlot
-# from utils.Timer import asMinutes, timeSince
-
-# from data.example_Data import Lang, prepareData
-# from model.rnn_model.encoderRNN import (
-#     EncoderLSTM, 
-# )
-# from model.rnn_model.decoderRNN import (
-#     AttnDecoderLSTM1,
-#     AttnDecoderLSTM2,
-# )
-# from model.seq2seq_Model import (
-#     Seq2Seq_batch_ptModel,
-#     Seq2SeqTranslate_ptTokenizer,
-#     PAD_token,
-# )
 
 from model_abc import (
     LSTM_text_classify_model,
@@ -43,9 +24,7 @@ from torch.utils.tensorboard import SummaryWriter
 class Batch_ExpTrain():
     def __init__(self, train_dataset, valid_dataset, device):
         self.train_dataset = train_dataset
-        # self.train_y = train_y
         self.valid_dataset = valid_dataset
-        # self.valid_y = valid_y
 
         self.device = device
 
@@ -67,9 +46,6 @@ class Batch_ExpTrain():
 
         # default `log_dir` is "runs" - we'll be more specific here
         writer = SummaryWriter(log_dir)
-        # print(model.encoder)
-        # writer.add_graph(model.encoder)
-        # writer.add_graph(model.decoder)
         
         ###  DataLoader  ###
         batch_num = len(self.train_dataset) / batch_size
@@ -82,13 +58,11 @@ class Batch_ExpTrain():
         min_loss = 1.0
         valid_acc_arr = []
         
-        ## train
         for epc in tqdm(range(epochs)):
-            # X_batchs, Y_batchs = self.xy2batch(self.train_x, self.train_y, batch_size)
+            ## train
             train_loss = 0
             train_acc = 0
             total_count = 0
-            # print('[Info] train!')
             for idx, (X_batch, Y_batch) in enumerate(train_dataloader):
                 loss_batch = 0
                 loss_batch, pred_batch_arr = model.fit(X_batch, Y_batch, criterion, optimizer, self.device)
@@ -101,43 +75,22 @@ class Batch_ExpTrain():
                 total_count += Y_batch.size(0)
             train_acc /= total_count
             
-            if min_loss > train_loss:
-                model.save()
-                min_loss = train_loss
-                print('[Info] model saved !')
-
             ## valid
             valid_acc = 0
             total_count = 0
-            # print('[Info] valid!')
             for idx, (X_batch, Y_batch) in enumerate(valid_dataloader):
                 valid_loss = 0
                 valid_loss, pred_batch_arr = model.predict(X_batch, Y_batch, criterion, self.device)
-                # acc を計算する。
                 _, pred_batch = torch.max(pred_batch_arr, 1)
-                # y_tensor = torch.tensor(Y_batchs[i], device=self.device)
                 # acc を計算する。
                 for j, ans in enumerate(Y_batch):
                     # print(pred_batch[j].item(), ans.item())
                     if pred_batch[j].item() == ans.item():
                         valid_acc += 1
-                
-                # valid_acc = metric(pred_batch_arr, self.valid_y)
-                # pred_deq.append(pred_batch_arr)
-                    
-                # valid_loss = 0
-                # valid_acc = 0
-                # for input_batch, input_lens, target_batch, target_lens in model.generate_batch(self.test_pairs, batch_size=batch_size, shuffle=False):
-                #     loss, acc = model.evaluate_batch(input_batch, input_lens,
-                #                                       target_batch, target_lens, criterion)
-                #     valid_loss += loss
-                #     valid_acc += acc
-                # validation_loss = valid_loss / (len(self.test_pairs) / batch_size)
-                # validation_acc = valid_acc / (len(self.test_pairs) / batch_size)
-            
                 total_count += Y_batch.size(0)
             valid_acc /= total_count
 
+            # tensorboard
             writer.add_scalar('train loss', train_loss, epc)
             writer.add_scalar('valid loss', valid_loss, epc)
             writer.add_scalar('train acc', train_acc, epc)
@@ -145,9 +98,19 @@ class Batch_ExpTrain():
 
             # print(f'[Info] train (loss, acc) : {train_loss}, {train_acc} \n    valid (loss, acc) : {valid_loss}, {valid_acc}')
 
+            # valid が最大の時の重みを保存
+            if min_loss > valid_acc:
+                model.save()
+                min_loss = valid_acc
+                print('[Info] model saved! (max valid)')
+            
+            # 最終 step の重みを保存
+            model.save('LSTM_classifier_latest.pth')
+
             valid_acc_arr.append(valid_acc)
             if max(valid_acc_arr[-early_stopping:]) < max(valid_acc_arr):
                 print('[Info] early stopping します。')
+                writer.close()
                 break
         
         writer.close()
--- Experiment/model_abc/LSTM_text_classify_model.py
+++ Experiment/model_abc/LSTM_text_classify_model.py
@@ -8,7 +8,7 @@ import torch.nn as nn
 
 class LSTM_TextClassifier_ptModel(nn.Module):
     def __init__(self, embedding_dim, hidden_dim, vocab_size, tagset_size,
-                        save_m_dir='_logs/test/', save_model_name='LSTM_classifier_.pth'):
+                        save_m_dir=None, save_m_file='LSTM_classifier_.pth'):
         super().__init__()
         self.hidden_dim = hidden_dim
         # <pad>の単語IDが0なので、padding_idx=0としている
@@ -18,9 +18,10 @@ class LSTM_TextClassifier_ptModel(nn.Module):
         self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
         self.softmax = nn.LogSoftmax()
 
-        self.save_m_dir = save_m_dir
-        os.makedirs(save_m_dir, exist_ok=True)
-        self.save_m_path = os.path.join(save_m_dir, save_model_name)
+        if save_m_dir is not None:
+            self.save_m_dir = save_m_dir
+            os.makedirs(save_m_dir, exist_ok=True)
+            self.save_m_path = os.path.join(save_m_dir, save_m_file)
 
 
     def forward(self, sentence):
@@ -70,15 +71,15 @@ class LSTM_TextClassifier_ptModel(nn.Module):
 
 
     def load_weights(self, load_m_path='_logs/test/LSTM_classifier.pth',):
-        if load_m_path is not None:
-            param = torch.load(load_m_path)
-            self.load_state_dict(param)
-            print(f'[info] {load_m_path} loaded !')
+        param = torch.load(load_m_path)
+        self.load_state_dict(param)
+        print(f'[info] {load_m_path} loaded !')
 
-    def save(self, save_f_path=None):
-        if save_f_path is None:
-            save_f_path = self.save_m_path
-        torch.save(self.state_dict(), save_f_path)
+    def save(self, save_m_file=None):
+        save_m_path = self.save_m_path
+        if save_m_file is not None:
+            save_m_path = os.path.join(self.save_m_dir, save_m_file)
+        torch.save(self.state_dict(), save_m_path)
 
 
 
